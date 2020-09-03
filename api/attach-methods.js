30dc00a39fa2da8e058ec31bfcce5449a0f3434f
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-09-03 16:29:10 +0200

    Using JSON as mockup data

--- api/attach-methods.js
+++ api/attach-methods.js
@@ -1,5 +1,5 @@
 module.exports = function (methods) {
-  const mockupData = require(`./${this._section}/mockup-data.js`)
+  const mockupData = require(`./${this._section}/mockup-data.json`)
   for (const method of methods) {
     this[method] = function (opt) {
       opt.Action = method.charAt(0).toUpperCase() + method.slice(1)
