52f0356c330ce8c07c52925de773d4c8c45b3882
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-09-03 17:28:30 +0200

    Updated set-env utility logic

--- test/utils/set-env.js
+++ test/utils/set-env.js
@@ -2,16 +2,10 @@ if (!process.env.GITHUB_ACTIONS) {
   const fs = require('fs')
   const path = require('path')
   const dotenv = require('dotenv')
-  const confPath = path.join(__dirname, '..', '..', 'test', '.env')
+  const confPath = path.join(__dirname, '..', '.env')
   if (!fs.existsSync(confPath)) {
     console.log('No environment configuration file found in test, stopping process...')
     process.exit()
   }
   dotenv.config({ path: confPath })
-} else {
-  const missingEnvVars = ['MWS_KEY_ID', 'MWS_KEY_SECRET', 'AMZ_EU_SELLER_ID'].filter(envVar => !process.env[envVar])
-  if (missingEnvVars.length > 0) {
-    console.log(`Missing environment ${missingEnvVars.length > 1 ? 'variables' : 'variable'} ${missingEnvVars.join(', ')}, stopping process...`)
-    process.exit()
-  }
 }
