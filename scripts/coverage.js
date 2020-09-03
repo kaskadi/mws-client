eee139ebc62cbe5fc733a39c921cd2a036f4eb5f
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-09-03 16:20:44 +0200

    Deleted node scripts for test/coverage

--- scripts/coverage.js
+++
@@ -1,6 +0,0 @@
-const spawnSync = require('child_process').spawnSync
-
-require('./utils/set-env.js')
-
-spawnSync('nyc', ['--reporter=html', 'mocha'], { stdio: 'inherit' })
-spawnSync('xdg-open', ['./coverage/index.html'], { stdio: 'inherit' })
